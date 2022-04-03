e044ede566cba6924729416684cbe5271a2ab42f
Babe Saleh Mahfoud <babesalehmahfoud@gmail.com>
2022-04-03 14:49:01 +0000

    added data

--- lib/models/subject.dart
+++ lib/models/subject.dart
@@ -10,3 +10,16 @@ class Subjects {
   final List<Subject> subjects;
   Subjects({required this.subjects});
 }
+
+class WhoAreWeData {
+  final String part1 =
+      'Babe Saleh is a computer programmer who decided to start a software startup. graduated from Laayoune High School., with a baccalaureate in Natural Sciences and currently majoring in Computer Engineering at the Higher School of Technology (EST) in Casablanca.';
+  final String part2 =
+      'He has an aspiring vision in software development business particularly app development. And in 04/10/2021  he launched his first app, Telmidi.';
+  final String part3 =
+      'Telmidi is a platform that helps high school students to master their curriculum in an easy and interactive way, it\'s free and always will be free.';
+  final String part4 =
+      'If you are a graphic designer, consider joining us by emailing: telmidi.contact@gmail.com.';
+  final String part5 =
+      'All images and vector materials used in this software, are attributed to vecteezy.com and  flaticons.com ';
+}
--- lib/screens/who_are_we_screen.dart
+++ lib/screens/who_are_we_screen.dart
@@ -2,6 +2,231 @@ import 'package:bac_note/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
+class WhoAreWe extends StatefulWidget {
+  @override
+  State<WhoAreWe> createState() => _WhoAreWeState();
+}
+
+class _WhoAreWeState extends State<WhoAreWe> {
+  final String _landingPage = "https://mohamed-abdelahi-haibelty.github.io";
+  final String _email =
+      "mailto:<babesalehmahfoud@gmail.com>?subject=Hello Telmidi&body=Hello";
+  final String _linkedin = 'linkedin.com/in/babe-saleh-mahfoud-519b52200/';
+  final String _facebook = "";
+  final String _call = "tel:+222 31 04 81 16";
+  Future<void> sendEmail(String type) async {
+    switch (type) {
+      case "website":
+        await canLaunch(_landingPage) ? launch(_landingPage) : print('error');
+
+        break;
+      case "email":
+        await launch(_email);
+        break;
+
+      case "facebook":
+        await launch(_facebook);
+        break;
+
+      case "call":
+        await launch(_call);
+        break;
+
+      case "linkedin":
+        await launch(_linkedin);
+        break;
+      default:
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: ReturnIconWidget(context),
+        elevation: kAppBarElevation,
+      ),
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 45.0),
+                  child: Text(
+                    'Telmidi',
+                    style: kMainLogo,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  color: kMainColor,
+                  padding: EdgeInsets.symmetric(horizontal: 38),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundImage:
+                            AssetImage('assets/images/whoarewe/saleh.jpg'),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Babe Saleh Mahfoud',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    SelectableText(
+                      WhoAreWeData().part1,
+                      style: kWhoAreWeStyle,
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(
+                      height: 68,
+                    ),
+                    SelectableText(
+                      WhoAreWeData().part2,
+                      style: kWhoAreWeStyle,
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(
+                      height: 68,
+                    ),
+                    SelectableText(
+                      WhoAreWeData().part3,
+                      style: kWhoAreWeStyle,
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(
+                      height: 68,
+                    ),
+                    SelectableText(
+                      WhoAreWeData().part4,
+                      style: kWhoAreWeStyle,
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(
+                      height: 38,
+                    ),
+                    SelectableText(
+                      WhoAreWeData().part5,
+                      style: kWhoAreWeStyle.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                color: kMainColor,
+                child: Column(
+                  children: [
+                    Text(
+                      'Contact Us',
+                      style: TextStyle(
+                          fontFamily: 'Telmidi',
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
+                          color: kSecondaryColor),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                            onTap: () {
+                              sendEmail("email");
+                            },
+                            child: Icon(FontAwesomeIcons.solidEnvelope,
+                                color: kSecondaryColor)),
+                        GestureDetector(
+                            onTap: () {
+                              sendEmail("website");
+                            },
+                            child:
+                                Icon(Icons.language, color: kSecondaryColor)),
+                        GestureDetector(
+                            onTap: () {
+                              sendEmail("facebook");
+                            },
+                            child: Icon(FontAwesomeIcons.facebookSquare,
+                                color: kSecondaryColor)),
+                        GestureDetector(
+                            onTap: () {
+                              sendEmail("call");
+                            },
+                            child: Icon(FontAwesomeIcons.phoneAlt,
+                                color: kSecondaryColor)),
+                        GestureDetector(
+                            onTap: () {
+                              sendEmail("linkedin");
+                            },
+                            child: Icon(FontAwesomeIcons.linkedin,
+                                color: kSecondaryColor)),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 70,
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 6.0),
+                        child: Text(
+                          'All Rights Reserved, Telmidi 2021.',
+                          style: TextStyle(
+                            fontFamily: 'Ubuntu',
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
