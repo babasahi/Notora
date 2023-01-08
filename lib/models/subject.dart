f8e7b691a35c06ee7c6b78797be79082e7991d54
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2023-01-08 22:19:15 +0100

    changes who are we page

--- lib/models/subject.dart
+++ lib/models/subject.dart
@@ -8,7 +8,7 @@ class Subject {
 
 class WhoAreWeData {
   final String part1 =
-      'Babe Saleh Mahfoud est un développeur d\'applications mobiles qui a décidé de démarrer une startup technologique pour produire des idées commerciales créatives. Diplômé du lycée de Laâyoune avec un baccalauréat en sciences naturelles et actuellement en majeure en génie informatique à l\'École Supérieure de Technologie (EST) de Casablanca.';
+      'Babe Saleh Mahfoud is a mobile application developer. Graduated from Lycée d\'Aioun with a degree in natural sciences and currently studying Computer Engineering at the Ecole Supérieure de Technologie (ESTC) in Casablanca.';
   final String part2 =
       'Il a une vision aspirante dans le domaine du développement de logiciels, en particulier le développement d\'applications. Et le 10/04/2021, il a lancé sa première application, Telmidi.';
   final String part3 =
